00e78b629f589e378cdf31ac1f693438f86680d5
Alfred Dagenais <jesuis@alfreddagenais.com>
2020-02-26 22:28:36 -0500

    CHG: Node version app to the last elements

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // module dependencies
 var express = require('express')
+	
+  , cookieParser = require('cookie-parser')
+  , favicon = require('serve-favicon')
+  , morgan = require('morgan')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+  , errorHandler = require('errorhandler')
+
   , http = require('http')
   , path = require('path')
   , socketio = require('socket.io')
@@ -13,20 +21,27 @@ var server = http.createServer(app);
 var io = socketio.listen(server);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(morgan('dev'));
+app.use(cookieParser());
+//app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+//app.use(methodOverride());
+//app.use(methodOverride('X-HTTP-Method-Override'));
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler());
 }
 
 // serve index and view partials
